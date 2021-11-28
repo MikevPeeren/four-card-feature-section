e0f9eb86802bbd5919f589bb6f00c6b9410a1689
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-28 11:42:15 +0100

    :lipstick: added colours to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#EA5454",
+        cyan: "#44D3D2",
+        orange: "#FCAE4A",
+        blue: "#549EF2",
+        gray: "#4D4F62"
+      }
+    },
   },
   variants: {
     extend: {},
