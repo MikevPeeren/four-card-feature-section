17410106a8ff3599c376779145be0b6bd1de8357
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-28 11:48:05 +0100

    :sparkles: added font and headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <h1>Reliable, efficient delivery <strong>Powered by Technology</strong></h1>
+          <h2>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</h2>
+        </div>
+
+        <div></div>
+      </main>
 
       <footer></footer>
     </div>
